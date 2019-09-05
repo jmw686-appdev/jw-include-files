919f1a1aa368bada69826430b1f3fa9799ad7a7f
jelaniwoods <jelani@firstdraft.com>
2019-09-05 12:39:57 -0500

    actually require the fools

--- lib/jw_fileinclude.rb
+++ lib/jw_fileinclude.rb
@@ -1,5 +1,10 @@
 require "jw_fileinclude/version"
 
+require "jw_fileinclude/active_record_relation_patch"
+require "jw_fileinclude/attribute_methods"
+require "jw_fileinclude/fetch_store_patch"
+require "jw_fileinclude/delegation_monkey_patch"
+
 module JwFileinclude
   class Error < StandardError; end
   # Your code goes here...
